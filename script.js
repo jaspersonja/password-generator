44827a03e63c0ccbef91f5b36749c2973624fddd
unknown <jacob.jasperson@yahoo.com>
2022-09-30 23:04:58 -0700

    added variables and beginning code to js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = [ " ", "!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[", "]", "^","_","`","{", "|","}","~"];
+var number = [ "1","2","3","4","5","6","7","8","9","0"];
+var random = []
+generateBtn = function(generateBtn.addEventListener("click", writePassword)){
+if (confirm("Would you like to use uppercase letters?") == true) {
+  random.push(uppercase)
+} else {
+  text = "No problem.";
+}
+if (confirm("Would you like to use lowercase letters?") == true) {
+  random.push(lowercase)
+} else {
+  text = "No problem.";
+}
+if (confirm("Would you like to use special characters?") == true) {
+  random.push(special)
+} else {
+  text = "No problem.";
+}
+if (confirm("Would you like to use numbers?") == true) {
+  random.push(number)
+} else {
+  text = "No problem.";
+}
 
+};
+console.log(random);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
